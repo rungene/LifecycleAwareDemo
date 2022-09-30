ffeb236acb8570f04056b867b3d1191c2eb449af
rungene <rungenesamuel@gmail.com>
2022-09-30 21:32:26 +0300

    Testing the observation of lifecycle owner throigh onCreate method

--- app/src/main/java/com/rungene/android/lifecycleawaredemo/MainActivityObserver.kt
+++ app/src/main/java/com/rungene/android/lifecycleawaredemo/MainActivityObserver.kt
@@ -1,6 +1,17 @@
 package com.rungene.android.lifecycleawaredemo
 
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
 class MainActivityObserver : LifecycleObserver{
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreateEvent() {
+        Log.i(TAG,"Observer On_CREATE")
+    }
+    companion object {
+        private val TAG : String = MainActivityObserver::class.java.simpleName
+    }
 }
