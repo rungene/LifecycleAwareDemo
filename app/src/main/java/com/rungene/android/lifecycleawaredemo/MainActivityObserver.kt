0b3b9c4992493e1e5daf921ff9ed349e8ca9fba7
rungene <rungenesamuel@gmail.com>
2022-10-01 05:39:15 +0300

    Adding the lifecycle events for respective recycle methods

--- app/src/main/java/com/rungene/android/lifecycleawaredemo/MainActivityObserver.kt
+++ app/src/main/java/com/rungene/android/lifecycleawaredemo/MainActivityObserver.kt
@@ -11,6 +11,26 @@ class MainActivityObserver : LifecycleObserver{
     fun onCreateEvent() {
         Log.i(TAG,"Observer On_CREATE")
     }
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStartEvent() {
+        Log.i(TAG,"Observer On_START")
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPauseEvent() {
+        Log.i(TAG,"Observer On_PAUSE")
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResumeEvent() {
+        Log.i(TAG,"Observer On_RESUME")
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroyEvent() {
+        Log.i(TAG,"Observer On_DESTROY")
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStopEvent() {
+        Log.i(TAG,"Observer On_Stop")
+    }
     companion object {
         private val TAG : String = MainActivityObserver::class.java.simpleName
     }
